591ede7a1cbcc5321c76acaef88ac0979fd20b96
Mohamd Imad <medoimad@hotmail.com>
2022-03-20 10:03:53 -0400

    basics1.py is an example file now we have imported three libraries for webscraping: requests bs4 lxml

---
+++ basics1.py
@@ -0,0 +1,24 @@
+#This .py file covers the basics of webscraping
+#Main Topics Covered:
+##The required packagesThe packages to install which are
+####requests, Allows us to make requests to websites and grab the info off the website
+###lxml, utilized by bs4 to divide the requests
+###bs4 "beautirful soup Version 4",
+
+#Step 1 Import the libraries
+import requests
+import bs4
+import lxml
+
+#Exp: Grab the page title of a www.leasebusters.com webpage
+
+def title():
+    pass
+
+
+
+
+
+
+if __name__ == '__main__':
+    title()
